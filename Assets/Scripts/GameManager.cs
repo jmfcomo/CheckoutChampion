44a1f02c4c99c1f472bc6f277b9a100afe8f93a9
NewKid25 <115599059+NewKid25@users.noreply.github.com>
2024-11-02 16:32:39 -0500

    gamemanager fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         DontDestroyOnLoad(this);
-        GameObjectEvents
+        //GameObjectEvents
     }
 
     [Serializable]
