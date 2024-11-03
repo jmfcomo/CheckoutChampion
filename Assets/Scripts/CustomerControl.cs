3cdf69152ff636bf822f5dd09a6d90ae80f32358
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2024-11-03 05:17:49 -0600

    customer dialogue

--- Assets/Scripts/CustomerControl.cs
+++ Assets/Scripts/CustomerControl.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CustomerControl : MonoBehaviour
@@ -9,6 +11,8 @@ public class CustomerControl : MonoBehaviour
     private int frame = 0;
     public float speed = 0.02f;
     private Animator animator;
+    private TMP_Text dialogueText;
+    public string dialogueString = "Hello world";
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,18 @@ public class CustomerControl : MonoBehaviour
         this.transform.position = spawnPoint.transform.position;
         animator = GetComponent<Animator>();
         animator.SetTrigger("TriStartWalk");
+
+        dialogueText = transform.Find("Dialogue").GetComponent<TMP_Text>();
+
+        dialogueText.enabled = true;
+
+        dialogueText.text = dialogueString;
+        dialogueText.alpha = 0f;
+
+        Debug.Log(dialogueString);
+
+        Invoke("fadeIn", 5f);
+        Invoke("fadeOut", 10f);
     }
 
     private void FixedUpdate()
@@ -32,6 +48,33 @@ public class CustomerControl : MonoBehaviour
         if (frame == totalFramesWalking)
         {
             animator.SetTrigger("TriEndWalk");
+        }
+    }
+
+    public void fadeIn()
+    {
+        StartCoroutine(fadeOverTime(0, 1, 1));
+    }
+
+    public void fadeOut()
+    {
+        StartCoroutine(fadeOverTime(1, 0, 1));
+    }
+    
+    public IEnumerator fadeOverTime(float fromVal, float toVal, float duration)
+    {
+        float counter = 0f;
+
+        while (counter < duration)
+        {
+            if (Time.timeScale == 0)
+                counter += Time.unscaledDeltaTime;
+            else
+                counter += Time.deltaTime;
+
+            dialogueText.alpha = Mathf.Lerp(fromVal, toVal, counter / duration);
+            Debug.Log("Val: " + dialogueText.alpha);
+            yield return null;
         }
     }
 
